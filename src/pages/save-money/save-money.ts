6907065558c1c5258631731206fc075c338a8281
faizal razak <mohamadfaizalrazak@gmail.com>
2018-05-08 13:20:38 +0800

    save-money page updated

--- src/pages/save-money/save-money.ts
+++ src/pages/save-money/save-money.ts
@@ -4,14 +4,6 @@ import {CardPaymentPage} from '../card-payment/card-payment';
 import { HttpProvider } from '../../providers/http/http'
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 
-
-/**
- * Generated class for the SaveMoneyPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-save-money',
